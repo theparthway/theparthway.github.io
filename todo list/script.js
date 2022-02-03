687eb23af867843295a921753d555f01abab98f3
Parth Shah <parthpsh@gmail.com>
2022-02-04 02:27:03 +0530

    Fixed todo list

--- todo list/script.js
+++ todo list/script.js
@@ -1,24 +1,25 @@
 const tb = document.querySelector('input[type="text"]');
-const btn = document.querySelector('.btn');
 const ul = document.querySelector('.list');
+const label = document.querySelector('.error');
 
-btn.addEventListener('click', function () {
-    let li = document.createElement('li');
-    let b = document.createElement('button');
-    li.textContent = tb.value;
-    b.textContent = "X";
-    ul.appendChild(li);
-    li.appendChild(b);
-    tb.value = "";
-    tb.focus();
-    b.addEventListener('click', function () {
-        li.remove();
-    })
-})
 
 tb.addEventListener('keyup', function(event) {
-    if (event.keyCode == 13) {
+    label.textContent = "";
+    if (event.keyCode === 13) {
         event.preventDefault();
-        btn.click();
+        if(tb.value == "") {
+            label.textContent = "Please enter some task";
+            return;
+        }
+        let li = document.createElement('li');
+        li.textContent = tb.value;
+        li.style.fontSize = "25px";
+        ul.appendChild(li);
+        tb.value = "";
+        tb.focus();
+        li.addEventListener('click', function () {
+            li.remove();
+            tb.focus();
+        })
     }
 })
