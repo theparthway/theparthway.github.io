d41d7f13f1ecaf4adccf0f53ded788e047787f37
theparthway <parthpsh@gmail.com>
2022-02-15 02:22:27 +0530

    Apod background

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const headerHamMenuCloseBtn = document.querySelector(
   '.header__main-ham-menu-close'
 )
 const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+const hero = document.querySelector('.home-hero');
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=nuTfpJELDwGbmcNupmJ3aD9FoCm17voCiOO8488Q').then(function(response) {
+  return response.json();
+}).then(function(data) {
+  if (data.media_type == 'image') {
+    hero.style.backgroundImage = 'url(' + data.url + ')';
+    console.log(data.url);
+  } else {
+    hero.style.backgroundImage = 'linear-gradient(to right, rgba(0, 98, 185, 0.8), rgba(0, 98, 185, 0.8)), url(../../assets/svg/common-bg.svg)';
+  }
+}).catch(function() {
+  hero.style.backgroundImage = 'linear-gradient(to right, rgba(0, 98, 185, 0.8), rgba(0, 98, 185, 0.8)), url(../../assets/svg/common-bg.svg)';
+  console.log("Booo");
+});
+
+hero.style.backgroundImage = 'url(./selfie.jpeg)';
 
 hamMenuBtn.addEventListener('click', () => {
   if (smallMenu.classList.contains('header__sm-menu--active')) {
