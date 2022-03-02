16d4414a1fbbaa1f39ed3b3ba560646ac96e2e50
theparthway <64493618+theparthway@users.noreply.github.com>
2022-03-02 08:52:29 +0530

    Added timings

--- time table/script.js
+++ time table/script.js
@@ -19,6 +19,7 @@ const col3 = document.querySelector(".col3");
 const now = document.querySelector('input[value="now"]');
 const full = document.querySelector('input[value="full"]');
 
+const timings = ['8:25-9:20', '9:25-10:20', '10:35-11:30', '11:35-12:30', '12:35-1:30', '1:35-2:30', '2:35-3:30', '3:35-4:30', '4:35-5:30'];
 const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 var currentTime = new Date();
