f65fcc889c82b3a9e89fffd7839d89acea66c9f5
theparthway <parthpsh@gmail.com>
2022-03-07 11:37:19 +0530

    Design changes

--- time table/script.js
+++ time table/script.js
@@ -1,19 +1,9 @@
-
-const radios = [];
-radios.push(document.querySelector('input[value="A1"]'));
-radios.push(document.querySelector('input[value="A2"]'));
-radios.push(document.querySelector('input[value="A3"]'));
-radios.push(document.querySelector('input[value="A4"]'));
-radios.push(document.querySelector('input[value="A5"]'));
-radios.push(document.querySelector('input[value="A6"]'));
-radios.push(document.querySelector('input[value="A7"]'));
-radios.push(document.querySelector('input[value="A8"]'));
-radios.push(document.querySelector('input[value="A9"]'));
-radios.push(document.querySelector('input[value="A10"]'));
+const container = document.querySelector(".container");
+const input = document.querySelector(".input");
+const section = document.querySelector(".section");
 
 const cols = [];
 
-const container = document.querySelector(".container");
 
 for (let i=0;i<9;i++) {
     let cell = document.createElement("div");
@@ -55,27 +45,18 @@ function setLabels(s) {
     }
     cols[hourParam].style.color = "rgb(43, 193, 243)";
     cols[hourParam].textContent = "➙  " + cols[hourParam].textContent;
-    // if (minute >= 30) {
-    //     col1.textContent = timings[hourParam - 1] + " | \n" + tt[secParam][hourParam];
-    //     col2.textContent = timings[hourParam] + " | \n" + tt[secParam][hourParam + 1];
-    //     col3.textContent = timings[hourParam + 1] + " | \n" + tt[secParam][hourParam + 2];
-    // } else if (minute < 30 && hour == 8) {
-    //     col1.textContent = "Good morning!";
-    //     col2.textContent = "Hope you have a nice day";
-    //     col3.textContent = timings[hourParam] + " | \n" + tt[secParam][hourParam + 1];
-    // } else {
-    //     col1.textContent = timings[hourParam - 1] + " | \n" + tt[secParam][hourParam - 1];
-    //     col2.textContent = timings[hourParam] + " | \n" + tt[secParam][hourParam];
-    //     col3.textContent = timings[hourParam + 1] + " | \n" + tt[secParam][hourParam + 1];
-    // }
 }
 
 setLabels(1);
 
-for (let i=0;i<radios.length;i++) {
-    radios[i].addEventListener('change', function() {
-        // day and section
-        // time
+const buttons = [];
+for (let i=0;i<10;i++) {
+    let button = document.createElement("button");
+    button.appendChild(document.createTextNode(tt[i][0]));
+    input.appendChild(button);
+
+    button.addEventListener('click', function() {
+        section.textContent = tt[i][0];
         setLabels(i);
     })
 }
