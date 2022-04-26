52f71242e096ed5f84c954f9c6a22dba25686154
theparthway <parthpsh@gmail.com>
2022-04-26 20:33:54 +0530

    Bug fixes and razor integration

--- dr2/script.js
+++ dr2/script.js
@@ -1,10 +1,11 @@
 const tb = document.getElementById("mobileNo");
 const btn = document.querySelector(".getStartedButton");
 var phoneno = /^\d{10}$/;
+var length = 0;
 
 function onlyNumberKey(evt) {
   // Only ASCII character in that range allowed
-  if (tb.value.length == 10) btn.disabled = false;
+  if (tb.value.length == 9) btn.disabled = false;
   else btn.disabled = true;
   var ASCIICode = evt.which ? evt.which : evt.keyCode;
   if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false;
@@ -22,6 +23,7 @@ window.addEventListener("load", function () {
       body: data,
     }).then(() => {
       alert("Our team will get back to you in 48 hours!");
+      window.location.href = "http://127.0.0.1:5500/dr2/getStarted.html";
     });
   });
 });
