72d4f2b4bb43f551c41d01ae096e010d0ec5f213
theparthway <parthpsh@gmail.com>
2022-11-26 03:26:42 +0530

    Added parameters

--- bubble/sketch1.js
+++ bubble/sketch1.js
@@ -1,27 +1,35 @@
 let values = [];
-let red = [];
+let comparisons = 0;
 
 let i = 0;
 let j = 0;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth - 20, windowHeight - 20);
     values = new Array(width);
-    colours = new Array(width);
     for (let i = 0; i < values.length; i++) {
         values[i] = random(height);
-        colours[i] = false;
     }
 }
 
 function draw() {
     background(0);
 
+    textSize(16);
+    fill(255);
+    strokeWeight(0.5);
+    textAlign(CENTER);
+    text('Bubble Sort Visualization', width / 2, 20);
+    textAlign(LEFT, CENTER);
+    text('Array Size: ' + width, 10, 20)
+    text('Comparisons: ' + comparisons, 10, 40);
+    text('Time elapsed: ' + (millis() / 1000), 10, 60);
+
     if (i < values.length) {
         for (let j = 0; j < values.length - i - 1; j++) {
             if (values[j] > values[j + 1]) {
                 swap(values, j, j + 1);
-                colours[j] = true;
+                comparisons++;
             }
         }
     } else {
@@ -31,11 +39,10 @@ function draw() {
     i++;
 
     for (let i = 0; i < values.length; i++) {
-        if (colours[j]) stroke(0, 255, 0);
-        else stroke(255);
+        stroke(255);
+        strokeWeight(1);
         // line(i, height, i, height - values[i]);
         circle(i, height - values[i], 1);
-        colours[i] = false;
     }
 }
 
