b6aaed54a07f888bf3999323f374a3db39339f7f
theparthway <parthpsh@gmail.com>
2022-12-19 22:01:38 +0530

    start button

--- viz/life/sketch.js
+++ viz/life/sketch.js
@@ -1,3 +1,11 @@
+
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+let button;
+let inGame;
+
 function make2DArray(cols, rows) {
 	let arr = new Array(cols);
 	for (let i = 0; i < arr.length; i++) {
@@ -5,11 +13,6 @@ function make2DArray(cols, rows) {
 	}
 	return arr;
 }
-
-let grid;
-let cols;
-let rows;
-let resolution = 10;
 
 function setup() {
 	createCanvas(windowWidth - 20, windowHeight - 190);
@@ -22,46 +25,50 @@ function setup() {
 		grid[i][j] = floor(random(2));
 		}
 	}
-	setInterval(next, 150);
+
+	button = createButton('start');
+	button.position(width / 2, height / 12);
+	button.mousePressed(start);
 }
 let color = (255);
 
 function draw() {
-};
-
-function next() {
 	background(0);
 	
 	for (let i = 0; i < cols; i++) {
 		for (let j = 0; j < rows; j++) {
-		let x = i * resolution;
-		let y = j * resolution;
-		if (grid[i][j] == 1) {
-			fill(0);
-			stroke(color);
-			rect(x, y, resolution - 1, resolution - 1);
-		}
-		}
-	}
-	
-	let next = make2DArray(cols, rows);
-	
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			let state = grid[i][j];
-			let neighbors = countNeighbors(grid, i, j);
-	
-			if (state == 0 && neighbors == 3) {
-				next[i][j] = 1;
-			} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-				next[i][j] = 0;
-			} else {
-				next[i][j] = state;
+			let x = i * resolution;
+			let y = j * resolution;
+			if (grid[i][j] == 1) {
+				fill(0);
+				stroke(color);
+				rect(x, y, resolution - 1, resolution - 1);
 			}
 		}
 	}
-	
-	grid = next;
+};
+
+function start() {
+	setInterval(function() {
+		let next = make2DArray(cols, rows);
+		
+		for (let i = 0; i < cols; i++) {
+			for (let j = 0; j < rows; j++) {
+				let state = grid[i][j];
+				let neighbors = countNeighbors(grid, i, j);
+		
+				if (state == 0 && neighbors == 3) {
+					next[i][j] = 1;
+				} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+					next[i][j] = 0;
+				} else {
+					next[i][j] = state;
+				}
+			}
+		}
+		
+		grid = next;
+	}, 150)
 
 }
 
